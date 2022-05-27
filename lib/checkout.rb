b75a1afe3d9c7c8a5c69ffc21e4d9fae78cf255f
Elliott Zaki <elliottzaki@Elliotts-Air.broadband>
2022-05-27 15:29:08 +0100

    Changed 'scanned_products' from an array to a hash. Changed the scan method to include an if/else statement that checks whether the two product numbers are the same, if they are they will add +1 to the quanitity number, if not it enters the object into the scanned products for the first time with a quantity of 1. - Scan tests amended and all passing.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -5,10 +5,15 @@ class Checkout
   attr_reader :scanned_products
 
   def initialize
-    @scanned_products = []
+    @scanned_products = {}
   end
 
   def scan(product)
-    @scanned_products << product
+    #scanned_products[001] = {:name 'lavender', :cost 9.25, :quantity 1}
+    if scanned_products.key?(product.product_number)
+      @scanned_products[product.product_number][:quantity] += 1
+    else
+      @scanned_products[product.product_number] = {name: product.name, cost: product.cost, quantity: 1}
+    end
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,14 +4,14 @@ describe Checkout do
   it { is_expected.to respond_to :scan }
   
   describe '.scan' do
-    it 'the scanned_products array should start empty' do
+    it 'the scanned_products hash should start empty' do
       expect(subject.scanned_products).to be_empty 
     end
 
     it 'is expected to scan product' do
       product = Product.new(001, 'Lavender_heart', 9.25)
       subject.scan(product)
-      expect(subject.scanned_products).to eq([product])
+      expect(subject.scanned_products[1]).to eq({:cost => 9.25, :name => 'Lavender_heart', :quantity => 1})
     end
   end
 end
